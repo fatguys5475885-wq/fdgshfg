abdcb4b3236147013ab28c654aa2a80fec9959f5
fatguys5475885-wq <fatguys5475885@gmail.com>
2025-11-22 01:21:50 +0700

    Create dggh.py

---
+++ dggh.py
@@ -0,0 +1,5 @@
+log_entries.append(msg)
+                    else:
+                        msg = f"⚠️ Không tìm thấy chuỗi trong: {full_path}"
+                        print(msg)
+                        log_entries.append(msg)
